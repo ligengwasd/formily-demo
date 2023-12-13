54950cc5508558f623792201b6da141f2ea72433
geng.li <geng.li@timschina.com>
2023-12-13 16:05:52 +0800

    feat(compiler): update

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,66 +1,127 @@
-import React from 'react';
-import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Alert, Typography } from 'antd';
-import { useIntl, FormattedMessage } from 'umi';
-import styles from './Welcome.less';
+import React, { useMemo } from 'react';
+import {
+  ComponentTreeWidget,
+  CompositePanel,
+  Designer,
+  DesignerToolsWidget,
+  HistoryWidget,
+  OutlineTreeWidget,
+  ResourceWidget,
+  SettingsPanel,
+  StudioPanel,
+  ToolbarPanel,
+  ViewPanel,
+  ViewportPanel,
+  ViewToolsWidget,
+  Workspace,
+  WorkspacePanel,
+} from '@designable/react';
+import {
+  ActionsWidget,
+  LogoWidget,
+  MarkupSchemaWidget,
+  PreviewWidget,
+  SchemaEditorWidget,
+} from '@designable/formily-antd/playground/widgets';
+import {
+  Card,
+  Cascader,
+  FormLayout,
+  NumberPicker,
+  ObjectContainer,
+  Password,
+  Rate,
+  Slider,
+  TreeSelect,
+} from '@designable/formily-antd';
+import { SettingsForm } from '@designable/react-settings-form';
+import { ArrayCards, ArrayTable } from '@designable/formily-antd/lib';
+import { createDesigner, Shortcut } from '@designable/core';
 
-const CodePreview: React.FC = ({ children }) => (
-  <pre className={styles.pre}>
-    <code>
-      <Typography.Text copyable>{children}</Typography.Text>
-    </code>
-  </pre>
-);
-
-const Welcome: React.FC = () => {
-  const intl = useIntl();
-
+const Edit: React.FC = () => {
+  const engine = useMemo(
+    () =>
+      createDesigner({
+        shortcuts: [
+          new Shortcut({
+            // codes: [
+            //   [KeyCode.Meta, KeyCode.S],
+            //   [KeyCode.Control, KeyCode.S],
+            // ],
+            // handler(ctx) {
+            //   saveSchema(ctx.engine)
+            // },
+          }),
+        ],
+        rootComponentName: 'Form',
+      }),
+    [],
+  );
   return (
-    <PageContainer>
-      <Card>
-        <Alert
-          message={intl.formatMessage({
-            id: 'pages.welcome.alertMessage',
-            defaultMessage: 'Faster and stronger heavy-duty components have been released.',
-          })}
-          type="success"
-          showIcon
-          banner
-          style={{
-            margin: -12,
-            marginBottom: 24,
-          }}
-        />
-        <Typography.Text strong>
-          <FormattedMessage id="pages.welcome.advancedComponent" defaultMessage="Advanced Form" />{' '}
-          <a
-            href="https://procomponents.ant.design/components/table"
-            rel="noopener noreferrer"
-            target="__blank"
-          >
-            <FormattedMessage id="pages.welcome.link" defaultMessage="Welcome" />
-          </a>
-        </Typography.Text>
-        <CodePreview>yarn add @ant-design/pro-table</CodePreview>
-        <Typography.Text
-          strong
-          style={{
-            marginBottom: 12,
-          }}
-        >
-          <FormattedMessage id="pages.welcome.advancedLayout" defaultMessage="Advanced layout" />{' '}
-          <a
-            href="https://procomponents.ant.design/components/layout"
-            rel="noopener noreferrer"
-            target="__blank"
-          >
-            <FormattedMessage id="pages.welcome.link" defaultMessage="Welcome" />
-          </a>
-        </Typography.Text>
-        <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
-      </Card>
-    </PageContainer>
+    <>
+      <Designer engine={engine}>
+        <StudioPanel logo={<LogoWidget />} actions={<ActionsWidget />}>
+          <CompositePanel>
+            <CompositePanel.Item title="panels.Component" icon="Component">
+              <ResourceWidget
+                title="sources.Inputs"
+                sources={[
+                  Password,
+                  NumberPicker,
+                  Rate,
+                  Slider,
+                  TreeSelect,
+                  Cascader,
+                  ObjectContainer,
+                ]}
+              />
+              <ResourceWidget title="sources.Layouts" sources={[Card, FormLayout]} />
+              <ResourceWidget title="sources.Arrays" sources={[ArrayCards, ArrayTable]} />
+              <ResourceWidget title="sources.Displays" sources={[Text]} />
+            </CompositePanel.Item>
+            <CompositePanel.Item title="panels.OutlinedTree" icon="Outline">
+              <OutlineTreeWidget />
+            </CompositePanel.Item>
+            <CompositePanel.Item title="panels.History" icon="History">
+              <HistoryWidget />
+            </CompositePanel.Item>
+          </CompositePanel>
+          <Workspace id="form">
+            <WorkspacePanel>
+              <ToolbarPanel>
+                <DesignerToolsWidget />
+                <ViewToolsWidget use={['DESIGNABLE', 'JSONTREE', 'MARKUP', 'PREVIEW']} />
+              </ToolbarPanel>
+              <ViewportPanel>
+                <ViewPanel type="DESIGNABLE">
+                  {() => (
+                    <ComponentTreeWidget
+                      components={{
+                        TreeSelect,
+                        Cascader,
+                        FormLayout,
+                        ObjectContainer,
+                      }}
+                    />
+                  )}
+                </ViewPanel>
+                <ViewPanel type="JSONTREE" scrollable={false}>
+                  {(tree, onChange) => <SchemaEditorWidget tree={tree} onChange={onChange} />}
+                </ViewPanel>
+                <ViewPanel type="MARKUP" scrollable={false}>
+                  {(tree) => <MarkupSchemaWidget tree={tree} />}
+                </ViewPanel>
+                <ViewPanel type="PREVIEW">{(tree) => <PreviewWidget tree={tree} />}</ViewPanel>
+              </ViewportPanel>
+            </WorkspacePanel>
+          </Workspace>
+          <SettingsPanel title="panels.PropertySettings">
+            <SettingsForm uploadAction="https://www.mocky.io/v2/5cc8019d300000980a055e76" />
+          </SettingsPanel>
+        </StudioPanel>
+      </Designer>
+    </>
   );
 };
 
-export default Welcome;
+export default Edit;
